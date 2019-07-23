9303aab26ba3788570cb6a7e34ae28e18caabe43
Rhys Bowen Harries <rbharries@gmail.com>
2019-07-23 16:55:20 +0100

    initial commit

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "eu-west-2",
+    BUCKET: "notes-app-api"
+  },
+  apiGateway: {
+    REGION: "eu-west-2",
+    URL: "https://dlkla7io5m.execute-api.eu-west-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "eu-west-2",
+    USER_POOL_ID: "eu-west-2_pixmDnCYE",
+    APP_CLIENT_ID: "58oq4seqq4jijugsbp6gbc90cu",
+    IDENTITY_POOL_ID: "eu-west-2:735b2a0c-0b86-4532-8169-8fa8be929e1a"
+  }
+};
